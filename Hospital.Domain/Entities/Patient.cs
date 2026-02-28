71884349fb0279b00d5b4b3c1a9d6e6b39b1797a
Avanshikha singh <singhavanshikha@gmail.com>
2026-02-28 15:57:18 +0530

    DAY1

---
+++ Hospital.Domain/Entities/Patient.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hospital.Domain.Entities
+{
+    internal class Patient
+    {
+    }
+}
---
+++ Hospital.Domain/Exception/DatabaseConnectionException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hospital.Domain.Exception
+{
+    internal class DatabaseConnectionException
+    {
+    }
+}
